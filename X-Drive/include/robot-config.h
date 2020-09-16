7cf0356cf1c15411ed16c5e15085aba2182e9b5e
Mangothing <67392361+Mangothing@users.noreply.github.com>
2020-09-16 16:46:38 -0400

    motor test

--- X-Drive/include/robot-config.h
+++ X-Drive/include/robot-config.h
@@ -2,9 +2,12 @@ using namespace vex;
 
 extern brain Brain;
 
+// VEXcode devices
+extern motor back_right_motor;
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void);
+void  vexcodeInit( void );
